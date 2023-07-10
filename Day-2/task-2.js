bd4cd7718e222bf0729b983256772e10e237e16b
Bukka sai vardhan <110643061+Bsaivardhan@users.noreply.github.com>
2023-07-10 18:08:14 +0530

    Update task-2.js

--- Day-2/task-2.js
+++ Day-2/task-2.js
@@ -2,8 +2,8 @@ function countMaxChoco(money, price)
 {
     let wrap=3;
     
-    if (money < price)
-        return 0;
+    if (money < price|| money<0)
+        return "Invalid input";
     let choc = parseInt(money / price, 10);
     choc = choc + parseInt((choc - 1) /(wrap - 1), 10);
     return choc;
